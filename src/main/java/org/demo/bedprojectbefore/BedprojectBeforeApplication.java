796c070119c58a98bb92f7da18d9260b09854353
hzy0711 <email@1633317531@11.com>
2021-03-29 21:45:38 +0800

    '第一次提交'

---
+++ src/main/java/org/demo/bedprojectbefore/BedprojectBeforeApplication.java
@@ -0,0 +1,13 @@
+package org.demo.bedprojectbefore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedprojectBeforeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedprojectBeforeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/demo/bedprojectbefore/BedprojectBeforeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.bedprojectbefore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedprojectBeforeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
