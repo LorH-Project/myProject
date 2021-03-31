c34e15ebfe48a695ef0ade968d27192e2599a20e
hzy0711 <email@1633317531@11.com>
2021-03-31 13:38:24 +0800

    success第一次添加

---
+++ src/main/java/org/demo/bedprojectbefore/UserController/LoginController.java
@@ -0,0 +1,15 @@
+package org.demo.bedprojectbefore.UserController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping(value = "/login")
+    public String login(){
+        return "登录成功";
+    }
+
+}
