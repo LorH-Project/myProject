02378ad5718a2c8e6fbea5e5ba9cd4de2bc5a0af
hzy0711 <email@1633317531@11.com>
2021-04-01 09:00:07 +0800

    controller

--- src/main/java/org/demo/bedprojectbefore/controller/UserController.java
+++ src/main/java/org/demo/bedprojectbefore/controller/UserController.java
@@ -1,4 +1,4 @@
-package org.demo.bedprojectbefore.UserController;
+package org.demo.bedprojectbefore.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
